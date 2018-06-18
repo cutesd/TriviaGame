e257d286d4b1f7187ee1d41d1d9fe933995ef4dd
Kerri Coupar <kimberly@kimberlycooper.me>
2018-06-17 20:23:38 -0700

    style finalized and js started

---
+++ assets/javascript/app.js
@@ -0,0 +1,228 @@
+$(document).ready(function () {
+
+    var PotterTrivia = function () {
+
+        var questions_arr = [
+            {
+                question: "That's why you're famous. That's why everybody knows your name. You're the boy who lived.",
+                answers: {
+                    a: "Minerva McGonagall",
+                    b: "Albus Dumbledore",
+                    c: "Rubeus Hagrid",
+                    d: "Hermione Granger"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "It's the same every year, packed with muggles of course.",
+                answers: {
+                    a: "Ron Weasley",
+                    b: "Molly Weasley",
+                    c: "Fred Weasley",
+                    d: "George Weasley"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "You've got dirt on your nose by the way. Did you know?",
+                answers: {
+                    a: "Hermione Granger",
+                    b: "Harry Potter",
+                    c: "Draco Malfoy",
+                    d: "Ginny Weasley"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Red hair and a hand-me-down robe... you must be a Weasley.",
+                answers: {
+                    a: "Draco Malfoy",
+                    b: "The Sorting Hat",
+                    c: "Hermione Granger",
+                    d: "Nearly Headless Nick"
+                },
+                correctAnswer: "a"
+            },
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    5. 'But everyone knows it's the Dark Arts he fancies. He's been after Quirrell's job for years.'
+    
+     Fred Weasley
+     Ron Weasley
+     Percy Weasley
+     Hermione Granger
+     /*{
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    6. 'Have you heard? Harry Potter's the new Seeker for Gryffindor. I always knew he'd do well.'
+    
+     Professor Dumbledore
+     Rubeus Hagrid
+     Argus Filch
+     Nearly Headless Nick
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    7. 'Stop, stop, stop, stop! You're going to take someone's eye out.'
+    
+     Neville Longbottom
+     Ron Weasley
+     Hermione Granger
+     Harry Potter
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    8. 'It shows nothing more or less than the deepest and most desperate desires of our hearts.'
+    
+    Answer: 
+     ( One Word, last name only)
+    9. 'Sorry, don't mean to be rude, but I'm in no fit state to entertain today.'
+    
+     Nearly Headless Nick
+     Percy Weasley
+     Hermione Granger
+     Hagrid
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    10. 'God, I miss the screaming.'
+    
+     Argus Filch
+     Professor Snape
+     Rubeus Hagrid
+     Draco Malfoy
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    11. 'You're a little bit scary sometimes you know, brilliant but scary.'
+    
+     Harry Potter
+     Hermione Granger
+     Neville Longbottom
+     Ron Weasley
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    12. 'Doesn't it seem a bit quiet to you?'
+    
+     Rubeus Hagrid
+     Ron Weasley
+     Harry Potter
+     Hermione Granger
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    13. 'You don't suppose this is going to be like real Wizard's Chess, do you?'
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    Answer: 
+     ( Two Words, first and last name)
+    14. 'Books and cleverness, there are more important things, like friendship and bravery.'
+    
+     Albus Dumbledore
+     Hermione Granger
+     Ron Weasley
+     Minerva McGonagall
+     {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+    15. Here's a harder quote to finish with. 'I have strength enough for this.'
+    
+     Lord Voldemort
+     Professor Snape
+     Professor Quirrell
+     Harry Potter
+      {
+                question: "",
+                answers: {
+                    a: "",
+                    b: "",
+                    c: "",
+                    d: ""
+                },
+                correctAnswer: ""
+            },
+            */
+        ];
+
+        this.play = function () {
+            console.log('HELLO!');
+        }
+    }
+
+    var myGame = new PotterTrivia();
+    myGame.play();
+
+});
